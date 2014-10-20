dcc14791d47bf93896ca7e6978d047a1065262ee
kaanant <kaanantt@gmail.com>
2014-10-20 23:28:51 +0300

    help

--- PL_AS2.c
+++ PL_AS2.c
@@ -1,46 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
     
-								
+void oteleGiris(long int *ptrdizi,int *toplamMisafir);							
 int main(){
 	int menu;
 	long int *ptrdizi;
 	int toplamMisafir=0;
 
-	ptrdizi=(long int *)malloc(sizeof(long int)*0);
+	ptrdizi=(long int *)malloc(0);
 	do{	
 	printf("	Menu\n1-oteleGiris\n2-oteldenCikis\n3-MisafirListesi\n4-EXIT\n");
 	scanf("%d",&menu);
-	}while(menu>4);	
+	
 	if (menu==1){
-		oteleGiris(*ptrdizi,toplamMisafir);	
+		oteleGiris(ptrdizi,toplamMisafir);	
 	}
 	else if(menu==2){
-		oteldenCikis()	
+		//oteldenCikis();
 	}
 	else if(menu==3){
-		misafirList()	
+		//misafirList();	
 	}
-	else if(menu==4){	
-	}	
+	else{}
+	}while(menu!=4);		
 	return 0;  
 }
  
-int oteleGiris(long int *ptrdizi,int *toplamMisafir){
+void oteleGiris(long int *ptrdizi,int *toplamMisafir){
 	long int TcNo;
 	printf("Misafir sayisini giriniz\n");
 	scanf("%d",&gelenMisafir);
 	newptr=(long int*)realloc(ptrdizi,toplamMisafir+gelenMisafir);
 	if(!newptr){
 		printf("Otelde Yer Yok");
-		return 0;
+		return ;
 	}
 	ptrdizi=newptr;
 	for(i=0;i<gelenMisafir;i++){
 		printf("Müsteri TC no\n);
 		scanf("%ld",&TcNo);
-		*ptrdizi+i=TcNo;		
+		*(ptrdizi+i)=TcNo;		
 	}
 	*toplamMisafir=toplamMisafir+gelenMisafir;
-	return 0;
+	return ;
 }

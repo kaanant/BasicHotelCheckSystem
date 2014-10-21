bf4a6dac3b69ef1d8b2243109c9b845e00f021b8
kaanant <kaanantt@gmail.com>
2014-10-21 15:38:15 +0300

    free

--- PL_AS2.c
+++ PL_AS2.c
@@ -1,47 +1,73 @@
 #include <stdio.h>
 #include <stdlib.h>
     
-void oteleGiris(long int *ptrdizi,int *toplamMisafir);							
+void oteleGiris(long int *,int *);		
+void oteldenCikis(long int *,int *);
+void misafirList(int ,long int *);
+
 int main(){
 	int menu;
 	long int *ptrdizi;
 	int toplamMisafir=0;
 
-	ptrdizi=(long int *)malloc(0);
-	do{	
-	printf("	Menu\n1-oteleGiris\n2-oteldenCikis\n3-MisafirListesi\n4-EXIT\n");
-	scanf("%d",&menu);
+	ptrdizi=(long int *)malloc(sizeof(long int));
+
+	do{
 	
-	if (menu==1){
-		oteleGiris(ptrdizi,toplamMisafir);	
-	}
-	else if(menu==2){
-		//oteldenCikis();
-	}
-	else if(menu==3){
-		//misafirList();	
-	}
-	else{}
+		printf("\t\tMenu\n1-oteleGiris\n2-oteldenCikis\n3-MisafirListesi\n4-EXIT\n");
+		scanf("%d",&menu);
+	
+		if (menu==1){
+			oteleGiris(ptrdizi,&toplamMisafir);	
+		}
+		else if(menu==2){
+			oteldenCikis(ptrdizi,&toplamMisafir);
+		}
+		else if(menu==3){
+			misafirList(toplamMisafir,ptrdizi);	
+		}
+		
 	}while(menu!=4);		
 	return 0;  
 }
  
-void oteleGiris(long int *ptrdizi,int *toplamMisafir){
-	long int TcNo;
-	int gelenMisafir,i;
+void oteleGiris(long int *ptrdizi,int *ptrToplamMisafir){
+	int gelenMisafir=0,i=0;
 	printf("Misafir sayisini giriniz\n");
 	scanf("%d",&gelenMisafir);
-	newptr=(long int*)realloc(ptrdizi,toplamMisafir+gelenMisafir);
+	long int *newptr=(long int*)realloc(ptrdizi,*ptrToplamMisafir+gelenMisafir);
 	if(!newptr){
 		printf("Otelde Yer Yok");
 		return ;
 	}
 	ptrdizi=newptr;
 	for(i=0;i<gelenMisafir;i++){
-		printf("Müsteri TC no\n);
-		scanf("%ld",&TcNo);
-		*(ptrdizi+i)=TcNo;		
+		printf("Müsteri TC no\n");
+		scanf("%ld",ptrdizi + *ptrToplamMisafir + i);
+				
 	}
-	*toplamMisafir=toplamMisafir+gelenMisafir;
+
+	*ptrToplamMisafir += gelenMisafir;
 	return ;
+}
+void oteldenCikis(long int *ptrdizi,int *toplamMisafir){
+	int cikanMisafir=0,i=0,j=0;
+	long int Tc=0;
+	printf("Otelden cikis yapan kisi sayisini giriniz");
+	scanf("%d",&cikanMisafir);
+	for(i=0;i<cikanMisafir;i++){
+		printf("cikanMisafir'in Tc no giriniz");
+		scanf("%ld",&Tc);
+		for(j=0;j<(*toplamMisafir);j++){
+			if(Tc==*(ptrdizi+j))
+				*(ptrdizi+j)=0;
+		}
+	}
+
+}
+void misafirList(int toplamMisafir,long int *ptrdizi){
+	int i;
+	for ( i=0;i<toplamMisafir;i++){
+		printf("%d--%ld\n",i+1,*(ptrdizi+i));
+	}
 }
